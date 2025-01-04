268bc73454216ef45c5ef281b699c8c4e2890474
duyphuongnguyen1999 <duyphuongnguyen1999@gmail.com>
2025-01-04 23:57:52 +0700

    Add implemtation of empty_tensor()

--- tensor.c
+++ tensor.c
@@ -1,36 +1,46 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <assert.h>
-#include "tensor_core.h"
+#include "tensor.h"
 
-// Global pointer for "this" instance
-static Tensor *this = NULL;
-
-Tensor *initialize_tensor(size_t ndims, size_t shape[])
+Tensor *empty_tensor(size_t shape[], size_t ndims, dtype dtype)
 {
-    assert(ndims >= 0);
+    assert(ndims >= 0 && "Number of dimensions must be equal or greater than zero");
+    // assert(shape != NULL && "shape array cannot be NULL");
+
     // Allocate memory for Tensor structure
-    this = (Tensor *)malloc(sizeof(Tensor));
+    Tensor *tensor = (Tensor *)malloc(sizeof(Tensor));
+    assert(tensor != NULL && "Memory allocation for Tensor failed");
 
-    if (ndims == 0) // Scalar tensors
-    {
-        this->shape = NULL;
-    }
+    tensor->shape = shape;
+    tensor->ndims = ndims;
 
-    this->ndims = ndims;
-    this->shape = shape;
-    size_t n_elements = 1;
+    // Allocate memory for strides
+    tensor->strides = (size_t *)malloc(tensor->ndims * sizeof(size_t));
+    assert(tensor->strides != NULL && "Memory allocation for strides failed");
+
+    // Initialize strides and calculate total number of elements
+    tensor->num_ele = 1;
     for (size_t i = 0; i < ndims; i++)
     {
-        n_elements *= shape[i];
+        tensor->num_ele *= shape[i];
     }
-    this->data = (float *)malloc(n_elements * sizeof(float));
-    this->grad = NULL;
-    this->require_grad = false;
+    tensor->stride[ndims - 1] = 1; // Last dimension stride is always 1
+    for (size_t i = ndims - 2; i >= 0; i--)
+    {
+        tensor->stride[i] = tensor->shape[i + 1] * tensor->strides[i + 1];
+    }
 
-    return this;
+    // Allocate memory for data (base on dtype.size)
+    tensor->data = (float *)malloc(tensor->num_ele * dtype.size);
+
+    // Initialize other tensor attributes
+    tensor->grads = NULL;
+    tensor->requires_grad = false;
+
+    return tensor;
 }
 
-float at(int position[])
+Tensor *zeros(size_t shape[], size_t ndims)
 {
 }
