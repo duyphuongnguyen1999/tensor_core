d95139d096dcdde1512f7c30969c75dba4a80723
duyphuongnguyen1999 <duyphuongnguyen1999@gmail.com>
2025-01-05 01:12:49 +0700

    Modify after renaming struct 'dtype' to 'Dtype'

--- tensor_core.h
+++ tensor_core.h
@@ -4,8 +4,8 @@
 #include "tensor.h"
 #include "dtype.h"
 
-Tensor *empty_tensor(size_t shape[], size_t ndims, dtype dtype);
-Tensor *zeros_tensor(size_t shape[], size_t ndims, dtype dtype);
-Tensor *ones_tensor(size_t shape[], size_t ndims, dtype dtype);
+Tensor *empty_tensor(size_t shape[], size_t ndims, Dtype dtype);
+Tensor *zeros_tensor(size_t shape[], size_t ndims, Dtype dtype);
+Tensor *ones_tensor(size_t shape[], size_t ndims, Dtype dtype);
 
 #endif
